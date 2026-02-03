eaa70e0bc706921616794c288a1dbd24f73fcca9
premraj81 <91459420+premraj81@users.noreply.github.com>
2026-02-03 14:42:15 +1300

    Add sumanth@xtra.co.nz to allowed users

--- src/lib/allowedUsers.ts
+++ src/lib/allowedUsers.ts
@@ -2,7 +2,8 @@ export const ALLOWED_USERS = [
     "premraj81@gmail.com",
     "81prem@gmail.com",
     "billyjar87@gmail.com",
-    "fiordlandpilotage@gmail.com"
+    "fiordlandpilotage@gmail.com",
+    "sumanth@xtra.co.nz"
 ];
 
 export function isUserAllowed(email: string): boolean {
